f1f248d8f856e9c453f392d421df84871d0f59e8
weezaard <weezaard@gmail.com>
2018-04-02 13:39:34 +0200

    Izklpaljam logiranje, ker smeti in ga po defaultu ne rabis.

--- configuration.js
+++ configuration.js
@@ -18,7 +18,8 @@ var Configuration = {
                 min: 0,
                 acquire: 30000,
                 idle: 10000
-            },            
+            },
+            logging: false         
         }
     },
 
