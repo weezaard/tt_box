1841b190692497716f674490cc1d2ae8efac27d8
weezaard <weezaard@gmail.com>
2018-04-23 14:18:23 +0200

    Implementacija 'naklon' instrumenta.

--- formula/naklon.js
+++ formula/naklon.js
@@ -2,14 +2,26 @@ const AbstractFormula = require('./AbstractFormula');
 
 class formula extends AbstractFormula {
 
+    getSlopeAngle(s1,s2) {
+        return Math.atan((s2[1] - s1[1]) / (s2[0] - s1[0])) * 180/Math.PI;
+    }
+      
     execute() {
+        let slv = this.context.getValues('SLV');
+        let indeks = this.context.getValues('index');
+        console.log(slv);
+        console.log(indeks);
+        for (let i in slv) {
+            if (i < 1) continue;
+            let x1 = indeks[i-1]
+            let y1 = slv[i-1];
+            let x2 = indeks[i];
+            let y2 = slv[i];
+            let slope = this.getSlopeAngle([x1, y1], [x2, y2]);
+            console.log(`i=${i}, x1=${x1}, y1=${y1}, x2=${x2}, y2=${y2}, slope=${slope}`);
+        }
     }
 
-    /*
-    slope(n) {
-        (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
-    }
-    */
 }
 
 module.exports = formula;
