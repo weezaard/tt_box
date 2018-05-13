ec097ad4f045d6a0c62bbf9a626c19281df3d4cf
weezaard <weezaard@gmail.com>
2018-05-13 21:04:15 +0200

    importing instruments bulk data

--- db.js
+++ db.js
@@ -2,6 +2,9 @@ const Sequelize = require('sequelize');
 const User = require('./model/User');
 const Config = require('./configuration.js');
 const Cache = require('./cache');
+const fs = require('fs');
+const es = require('event-stream');
+const csvParser = require('csv-parse/lib/sync');
 
 const Asset = require('./model/Asset');
 const Instrument = require('./model/Instrument');
@@ -12,6 +15,10 @@ const util = require('./modules/util.js');
 
 var syncOptions = {
     force: true
+};
+
+var syncOptionsNoForce = {
+    force: false
 };
 
 /**
@@ -26,7 +33,7 @@ PropertyValue.belongsTo(Asset);
 
 module.exports.syncDb = async function() {
     let promiseAsset = Asset.sync(syncOptions).then(() => {
-        return Instrument.sync(syncOptions);    
+        return Instrument.sync(syncOptionsNoForce);    
     });
 
     let promiseProperty = Property.sync(syncOptions).then(() => {
@@ -68,17 +75,78 @@ async function saveValues(propertyName, values, asset) {
     }));
 }
 
-module.exports.importInstrumentsForAsset = async function(data, asset_name) {
+module.exports.getLastInstrument = async function(assetName) {
+    let lastInstrument = await Instrument.findAll({ 
+        limit: 1,
+        order: [ [ 'date_of_value', 'DESC' ] ]
+    });
+    return lastInstrument[0];
+}
+
+module.exports.importInstrumentsFromCsv = async function(fileName, assetName) {
+    let asset = Cache.assetCache.get(assetName);
+    let lineNr = 0;
+    let allPromises = [];
+    var s = fs.createReadStream(fileName);
+    var s2 = s.pipe(es.split());
+    var map = es.mapSync(function(line) {
+        if (line==null || line=='') return;
+        // pause the readstream
+        s.pause();
+        lineNr += 1;
+        if (lineNr % 100 == 0) console.log(lineNr);
+        var parsedLine = csvParser(line)[0];
+        let ts = parsedLine[0];
+        let dt = new Date(ts*1000);
+        let price = parseFloat(parsedLine[1]);
+        //let volume = parseFloat(parsedLine[2]);
+        //console.log(`Inserting date_of_value: ${dt}, unix_ts: ${ts}, value: ${price}, index ${lineNr}, asset_name: ${asset.name}`);
+        let newInstrument = Instrument.create({
+            date_of_value: dt,
+            unix_ts : ts,
+            value: price,
+            index: lineNr-1,
+            asset_name: asset.name
+        }).catch((err) => { 
+            err.indeks = lineNr;
+            err.inData = line;
+            throw err;
+        });
+        allPromises.push(newInstrument);
+       s.resume();
+    });
+    var s3 = s2.pipe(map);
+    await streamToPromise(s3);  // wait for stream to finish
+    return Promise.all(allPromises);    // Promise for all create Instrument statements (should wait for it)
+}
+
+function streamToPromise(stream) {
+    return new Promise(function(resolve, reject) {
+        stream.on("end", resolve);
+        stream.on("error", reject);
+    });
+}
+
+/**
+ * Appends the data to the database, without checking if data already exists in the db.
+ * Dummy but fast.
+ * @param {*} data 
+ * @param {*} asset_name 
+ */
+module.exports.appendInstrumentsForAsset = async function(data, asset_name, lastIndex) {
     var asset = Cache.assetCache.get(asset_name);
     //console.log('asset_name='+asset_name);
     //console.log('cache='+Cache.assetCache.get('BTC').name);
 
     return Promise.all(data.map((entry, i, origData) => {
         //console.log(`value at ${i}: ${entry.val}`);
+        let newIndex = parseFloat(i + parseFloat(lastIndex) + 1);
+        //console.log(`newIndex=${newIndex}`);
         let newInstrument = Instrument.create({
-            date_of_value: util.parseXlsxDate(entry.date),
+            date_of_value: entry.date,
+            unix_ts : entry.unix_ts,
             value: parseFloat(entry.val),
-            index: i,
+            index: newIndex,
             asset_name: asset.name
         }).catch((err) => { 
             //return err 
